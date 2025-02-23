40411668c98562b27f74e82e0f8188b18a2308ea
ganesh <ganeshhgunda123@gmail.com>
2025-02-23 23:07:55 +0530

    Initial commit and defined some functions

---
+++ main.py
@@ -0,0 +1,18 @@
+def bin_to_hex():
+    pass
+
+def bin_to_dec():
+    pass
+
+def dec_to_bin():
+    pass
+
+def dec_to_hex():
+    pass
+
+def hex_to_bin():
+    pass
+
+def hex_to_dec():
+    pass
+
